6b135098ab070a604809322c220bb733fa0ac8fd
louislhotte <louis.lhotte@student-cs.fr>
2022-03-07 15:53:45 +0100

    Viewer for powsybl-network-area-diagram
    Signed-off-by: Florian Dupuy <florian.dupuy@rte-france.com>
    
    Co-authored-by: louislhotte <louis.lhotte@student-cs.fr>

---
+++ network-area-diagram-viewer/src/main/java/com/powsybl/nad/viewer/controller/DiagramController.java
@@ -0,0 +1,161 @@
+/**
+ * Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.nad.viewer.controller;
+
+import com.google.common.io.ByteStreams;
+import com.powsybl.iidm.network.Container;
+import com.powsybl.iidm.network.Network;
+import com.powsybl.iidm.network.Substation;
+import com.powsybl.iidm.network.VoltageLevel;
+import com.powsybl.nad.NetworkAreaDiagram;
+import com.powsybl.nad.svg.iidm.NominalVoltageStyleProvider;
+import javafx.beans.property.StringProperty;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
+import javafx.concurrent.Worker;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.ScrollEvent;
+import javafx.scene.web.WebView;
+import netscape.javascript.JSObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+/**
+ * @author Florian Dupuy <florian.dupuy at rte-france.com>
+ */
+public class DiagramController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DiagramController.class);
+
+    @FXML
+    public WebView diagramWebView;
+    @FXML
+    public TextArea svgContent;
+    @FXML
+    public TextArea info;
+
+    private String html;
+    private String js;
+
+    @FXML
+    private void initialize() throws IOException {
+        // Add Zoom management
+        diagramWebView.addEventFilter(ScrollEvent.SCROLL, (ScrollEvent e) -> {
+            if (e.isControlDown()) {
+                double deltaY = e.getDeltaY();
+                double zoom = diagramWebView.getZoom();
+                if (deltaY < 0) {
+                    zoom /= 1.1;
+                } else if (deltaY > 0) {
+                    zoom *= 1.1;
+                }
+                diagramWebView.setZoom(zoom);
+                e.consume();
+            }
+        });
+
+        // Avoid the useless right click on the image
+        diagramWebView.setContextMenuEnabled(false);
+
+        // Set up the listener on WebView changes
+        // A listener has to be added as loading takes time - execute once the content is successfully loaded
+        diagramWebView.getEngine().getLoadWorker().stateProperty().addListener((observable, oldValue, newValue) -> {
+            if (Worker.State.SUCCEEDED == newValue) {
+                JSObject window = (JSObject) diagramWebView.getEngine().executeScript("window");
+                window.setMember("jsHandler", new Object());
+            }
+        });
+
+        html = new String(ByteStreams.toByteArray(Objects.requireNonNull(getClass().getResourceAsStream("/svg.html"))));
+        js = new String(ByteStreams.toByteArray(Objects.requireNonNull(getClass().getResourceAsStream("/svg.js"))));
+    }
+
+    public void createDiagram(Model model, StringProperty modelSvgContent, Container<?> container) {
+        modelSvgContent.addListener((obs, oldV, newV) -> {
+            String embeddedSvg = html.replaceFirst("%__SVG__%", newV).replaceFirst("%__JS__%", js);
+            diagramWebView.getEngine().loadContent(embeddedSvg);
+        });
+        svgContent.textProperty().bind(modelSvgContent);
+
+        updateDiagram(model, modelSvgContent, container);
+    }
+
+    public static void updateDiagram(Model model, StringProperty modelSvgContent, Container<?> container) {
+        StringWriter writer = new StringWriter();
+        Service<String> nadService = new Service<>() {
+            @Override
+            protected Task<String> createTask() {
+                return new Task<>() {
+                    @Override
+                    protected String call() {
+                        NetworkAreaDiagram nad = getNetworkAreaDiagram(model, container);
+                        nad.draw(writer, model.getSvgParameters(), model.getLayoutParameters(),
+                                new NominalVoltageStyleProvider(model.getNetwork()),
+                                model.getLabelProvider(), model.getLayoutFactory());
+                        return writer.toString();
+                    }
+                };
+            }
+        };
+
+        nadService.setOnSucceeded(event -> modelSvgContent.setValue((String) event.getSource().getValue()));
+        nadService.setOnFailed(event -> {
+            Throwable exception = event.getSource().getException();
+            LOGGER.error(exception.toString(), exception);
+        });
+        nadService.start();
+    }
+
+    private static NetworkAreaDiagram getNetworkAreaDiagram(Model model, Container<?> container) {
+        switch (container.getContainerType()) {
+            case NETWORK:
+                return new NetworkAreaDiagram((Network) container);
+            case SUBSTATION:
+                List<String> vls = ((Substation) container).getVoltageLevelStream().map(VoltageLevel::getId).collect(Collectors.toList());
+                return new NetworkAreaDiagram(model.getNetwork(), vls, model.getDepth());
+            case VOLTAGE_LEVEL:
+                return new NetworkAreaDiagram(model.getNetwork(), container.getId(), model.getDepth());
+            default:
+                throw new AssertionError();
+        }
+    }
+
+    public void onClickFitToContent() {
+        String svgData = svgContent.getText();
+        Optional<String> svgLine = svgData.lines().filter(l -> l.contains("<svg")).findAny();
+        if (svgLine.isPresent()) {
+            String valuePattern = "\"([^\"]*)\"";
+            Pattern pW = Pattern.compile("width=" + valuePattern);
+            Matcher mW = pW.matcher(svgLine.get());
+            Pattern pH = Pattern.compile("height=" + valuePattern);
+            Matcher mH = pH.matcher(svgLine.get());
+            if (mH.find() && mW.find()) {
+                double svgWidth = Double.parseDouble(mW.group(1));
+                double svgHeight = Double.parseDouble(mH.group(1));
+                double paneWidth = diagramWebView.widthProperty().get();
+                double paneHeight = diagramWebView.heightProperty().get();
+                double zoomH = paneHeight / svgHeight;
+                double zoomW = paneWidth / svgWidth;
+                diagramWebView.setZoom(Math.min(zoomH, zoomW));
+            }
+        }
+    }
+
+    public void onClickResetZoom() {
+        diagramWebView.setZoom(1.0);
+    }
+}
---
+++ network-area-diagram-viewer/src/main/java/com/powsybl/nad/viewer/controller/MainViewController.java
@@ -0,0 +1,379 @@
+/**
+ * Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.nad.viewer.controller;
+
+import com.powsybl.iidm.import_.Importers;
+import com.powsybl.iidm.network.*;
+import com.powsybl.loadflow.LoadFlow;
+import com.powsybl.nad.viewer.NetworkAreaDiagramViewer;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.CheckBoxTreeCell;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.FileChooser;
+import javafx.util.StringConverter;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+import java.util.function.Function;
+import java.util.prefs.Preferences;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @author Florian Dupuy <florian.dupuy at rte-france.com>
+ */
+public class MainViewController implements ChangeListener<Object> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MainViewController.class);
+
+    public static final String CASE_PATH_PROPERTY = "casePath";
+    public static final String CASE_FOLDER_PROPERTY = "caseFolder";
+
+    private final Preferences preferences = Preferences.userNodeForPackage(NetworkAreaDiagramViewer.class);
+
+    @FXML
+    public TextField filePath;
+    @FXML
+    public TextField filterField;
+    @FXML
+    public TreeView<Container<?>> vlTree;
+    @FXML
+    public CheckBox hideVoltageLevels;
+    @FXML
+    public CheckBox hideSubstations;
+    @FXML
+    public CheckBox showNames;
+    @FXML
+    public Spinner<Integer> depthSpinner;
+    @FXML
+    public Spinner<Double> springRepulsionSpinner;
+    @FXML
+    public TabPane checkedTab;
+    @FXML
+    public BorderPane selectedDiagram;
+    @FXML
+    public ChoiceBox<String> labelProviderChoice;
+    @FXML
+    public ChoiceBox<String> layoutChoice;
+    @FXML
+    public CheckBox infoAlongEdge;
+    @FXML
+    public TabPane checkedOrSelected;
+    @FXML
+    private Node loadingStatus;
+    @FXML
+    private DiagramController selectedDiagramController;
+
+    private final Map<Tab, DiagramController> checkedDiagramControllers = new HashMap<>();
+    private Model model;
+
+    @FXML
+    private void initialize() {
+        String casePathPropertyValue = preferences.get(CASE_PATH_PROPERTY, null);
+        if (casePathPropertyValue != null) {
+            loadFile(new File(casePathPropertyValue));
+        }
+
+        model = new Model(depthSpinner.valueProperty(),
+                layoutChoice.valueProperty(),
+                labelProviderChoice.valueProperty(),
+                springRepulsionSpinner.valueProperty(),
+                infoAlongEdge.selectedProperty(),
+                showNames.selectedProperty());
+
+        model.getNetworkProperty().addListener((observableValue, oldNetwork, newNetwork) ->
+                initSubstationsTree(newNetwork));
+
+        depthSpinner.valueProperty().addListener(this);
+        layoutChoice.valueProperty().addListener(this);
+        labelProviderChoice.valueProperty().addListener(this);
+        springRepulsionSpinner.valueProperty().addListener(this);
+        infoAlongEdge.selectedProperty().addListener(this);
+        showNames.selectedProperty().addListener(this);
+
+        vlTree.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            model.setSelectedContainer(newValue.getValue());
+            selectedDiagramController.createDiagram(model, model.getSvgContent(), newValue.getValue());
+        });
+
+        vlTree.setCellFactory(param -> {
+            CheckBoxTreeCell<Container<?>> treeCell = new CheckBoxTreeCell<>();
+            StringConverter<TreeItem<Container<?>>> strConvert = new StringConverter<>() {
+                @Override
+                public String toString(TreeItem<Container<?>> c) {
+                    if (c.getValue().getContainerType() == ContainerType.NETWORK) {
+                        return "Full Network";
+                    }
+                    if (c.getValue() != null) {
+                        return showNames.isSelected() ? c.getValue().getNameOrId() : c.getValue().getId();
+                    }
+                    return "";
+                }
+
+                @Override
+                public TreeItem<Container<?>> fromString(String string) {
+                    return null;
+                }
+            };
+            treeCell.setConverter(strConvert);
+            return treeCell;
+        });
+    }
+
+    @Override
+    public void changed(ObservableValue<?> observable, Object oldValue, Object newValue) {
+        updateAllDiagrams();
+    }
+
+    private void updateAllDiagrams() {
+        if (model.getSelectedContainer() != null) {
+            DiagramController.updateDiagram(model, model.getSvgContent(), model.getSelectedContainer());
+        }
+        model.getCheckedContainerStream().forEach(container -> DiagramController.updateDiagram(model, model.getCheckedSvgContent(container), container));
+    }
+
+    @FXML
+    private void onClickLoadFile(MouseEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        String caseFolderPropertyValue = preferences.get(CASE_FOLDER_PROPERTY, null);
+        if (caseFolderPropertyValue != null) {
+            fileChooser.setInitialDirectory(new File(caseFolderPropertyValue));
+        }
+        fileChooser.setTitle("Open case File");
+        loadFile(fileChooser.showOpenDialog(loadingStatus.getScene().getWindow()));
+        event.consume();
+    }
+
+    public void loadFile(File file) {
+        if (file != null) {
+            Service<Network> networkService = new Service<>() {
+                @Override
+                protected Task<Network> createTask() {
+                    return new Task<>() {
+                        @Override
+                        protected Network call() {
+                            return Importers.loadNetwork(file.toPath());
+                        }
+                    };
+                }
+            };
+            handleLoadingResult(file, networkService);
+            networkService.start();
+        }
+    }
+
+    private void handleLoadingResult(File file, Service<Network> networkService) {
+        networkService.setOnRunning(event -> {
+            loadingStatus.setStyle("-fx-background-color: yellow");
+            filePath.setText(file.getAbsolutePath());
+            preferences.put(CASE_FOLDER_PROPERTY, file.getParent());
+        });
+
+        networkService.setOnSucceeded(event -> {
+            clean();
+            model.setNetwork((Network) event.getSource().getValue());
+            loadingStatus.setStyle("-fx-background-color: green");
+            preferences.put(CASE_PATH_PROPERTY, file.getAbsolutePath());
+        });
+
+        networkService.setOnFailed(event -> {
+            Throwable exception = event.getSource().getException();
+            LOGGER.error(exception.toString(), exception);
+            filePath.setText("");
+            loadingStatus.setStyle("-fx-background-color: red");
+        });
+    }
+
+    private void clean() {
+        filterField.setText("");
+        checkedTab.getTabs().clear();
+        model.setSelectedContainer(null);
+        model.setSvgContent("");
+    }
+
+    @FXML
+    private void onClickLoadFlow(MouseEvent actionEvent) {
+        LoadFlow.run(model.getNetwork());
+        updateAllDiagrams(); // forces update -> better done with NetworkListener
+    }
+
+    @FXML
+    private void initSubstationsTree() {
+        initSubstationsTree(model.getNetwork());
+    }
+
+    private void initSubstationsTree(Network network) {
+        if (network == null) {
+            return;
+        }
+
+        CheckBoxTreeItem<Container<?>> rootTreeItem = (CheckBoxTreeItem<Container<?>>) vlTree.getRoot();
+        if (rootTreeItem == null) {
+            rootTreeItem = new CheckBoxTreeItem<>(network);
+            rootTreeItem.setIndependent(true);
+            rootTreeItem.setExpanded(true);
+            vlTree.setRoot(rootTreeItem);
+        }
+
+        Set<String> containersChecked = rootTreeItem.getChildren().stream()
+                .flatMap(s -> Stream.concat(Stream.of(s), s.getChildren().stream()))
+                .filter(CheckBoxTreeItem.class::isInstance).map(ti -> (CheckBoxTreeItem<Container<?>>) ti)
+                .filter(CheckBoxTreeItem::isSelected)
+                .map(item -> item.getValue().getId())
+                .collect(Collectors.toSet());
+        TreeItem<Container<?>> selectedItem = vlTree.getSelectionModel().getSelectedItem();
+        String selectedContainerId = selectedItem == null ? null : selectedItem.getValue().getId();
+
+        rootTreeItem.getChildren().clear();
+        rootTreeItem.setValue(network);
+
+        String filter = filterField.getText();
+        for (Substation s : network.getSubstations()) {
+            CheckBoxTreeItem<Container<?>> sItem = null;
+            boolean sFilterOk = testPassed(filter, s);
+            List<VoltageLevel> voltageLevels = s.getVoltageLevelStream()
+                    .filter(v -> sFilterOk || testPassed(filter, v))
+                    .collect(Collectors.toList());
+            if ((sFilterOk || !voltageLevels.isEmpty()) && !hideSubstations.isSelected()) {
+                sItem = new CheckBoxTreeItem<>(s);
+                sItem.setIndependent(true);
+                sItem.setExpanded(true);
+                if (containersChecked.contains(s.getId())) {
+                    sItem.setSelected(true);
+                }
+                rootTreeItem.getChildren().add(sItem);
+                addListenerOnContainerItem(sItem);
+            }
+
+            initVoltageLevelsTree(rootTreeItem, sItem, voltageLevels, containersChecked);
+        }
+
+        List<VoltageLevel> emptySubstationVoltageLevels = network.getVoltageLevelStream()
+                .filter(v -> v.getSubstation().isEmpty())
+                .filter(v -> testPassed(filter, v))
+                .collect(Collectors.toList());
+        initVoltageLevelsTree(rootTreeItem, null, emptySubstationVoltageLevels, containersChecked);
+
+        rootTreeItem.getChildren().stream()
+                .flatMap(s -> Stream.concat(Stream.of(s), s.getChildren().stream()))
+                .filter(item -> item.getValue().getId().equals(selectedContainerId))
+                .findFirst()
+                .ifPresentOrElse(item -> vlTree.getSelectionModel().select(item),
+                        () -> vlTree.getSelectionModel().clearSelection());
+        vlTree.setShowRoot(true);
+    }
+
+    private void initVoltageLevelsTree(TreeItem<Container<?>> rootItem, CheckBoxTreeItem<Container<?>> sItem,
+                                       Collection<VoltageLevel> voltageLevels, Set<String> checkecContainers) {
+
+        for (VoltageLevel v : voltageLevels) {
+            if (!hideVoltageLevels.isSelected()) {
+                CheckBoxTreeItem<Container<?>> vItem = new CheckBoxTreeItem<>(v);
+                vItem.setIndependent(true);
+                if (checkecContainers.contains(v.getId())) {
+                    vItem.setSelected(true);
+                }
+                (sItem != null ? sItem : rootItem).getChildren().add(vItem);
+                addListenerOnContainerItem(vItem);
+            }
+        }
+    }
+
+    private Function<Identifiable<?>, String> getIdentifiableStringSupplier() {
+        return showNames.isSelected() ? Identifiable::getNameOrId : Identifiable::getId;
+    }
+
+    private boolean testPassed(String filter, Identifiable<?> identifiable) {
+        return StringUtils.isEmpty(filter)
+                || getIdentifiableStringSupplier().apply(identifiable)
+                .toLowerCase(Locale.getDefault())
+                .contains(filter.toLowerCase(Locale.getDefault()));
+    }
+
+    private void addListenerOnContainerItem(CheckBoxTreeItem<Container<?>> containerTreeItem) {
+        containerTreeItem.selectedProperty().addListener((obs, oldVal, newVal) -> {
+            if (Boolean.TRUE.equals(newVal)) {
+                createCheckedTab(containerTreeItem);
+            }
+        });
+    }
+
+    public void createCheckedTab(CheckBoxTreeItem<Container<?>> containerTreeItem) {
+        Container<?> container = containerTreeItem.getValue();
+        String tabName = getIdentifiableStringSupplier().apply(container);
+        List<Tab> tabList = checkedTab.getTabs();
+        if (tabList.stream().map(Tab::getText).noneMatch(tabName::equals)) {
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader();
+                Parent diagram = fxmlLoader.load(Objects.requireNonNull(getClass().getResourceAsStream("/diagramView.fxml")));
+                DiagramController checkedDiagramController = fxmlLoader.getController();
+                checkedDiagramController.createDiagram(model, model.getCheckedSvgContent(container), container);
+                Tab newCheckedTab = new Tab(tabName, diagram);
+                checkedDiagramControllers.put(newCheckedTab, checkedDiagramController);
+                newCheckedTab.setId(container.getId());
+                newCheckedTab.setOnClosed(event -> {
+                    containerTreeItem.setSelected(false);
+                    checkedDiagramControllers.remove(newCheckedTab);
+                });
+                containerTreeItem.selectedProperty().addListener(getChangeListener(newCheckedTab, containerTreeItem));
+                newCheckedTab.setTooltip(new Tooltip(container.getNameOrId()));
+                tabList.add(newCheckedTab);
+                checkedOrSelected.getSelectionModel().selectLast();
+                checkedTab.getSelectionModel().selectLast();
+            } catch (IOException e) {
+                LOGGER.error(e.toString(), e);
+            }
+        }
+    }
+
+    private ChangeListener<Boolean> getChangeListener(Tab tab, CheckBoxTreeItem<Container<?>> containerTreeItem) {
+        return new ChangeListener<>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                if (Boolean.FALSE.equals(newValue)) {
+                    checkedTab.getTabs().remove(tab);
+                    containerTreeItem.selectedProperty().removeListener(this);
+                    model.removeSvgContent(containerTreeItem.getValue());
+                    checkedDiagramControllers.remove(tab);
+                }
+            }
+        };
+    }
+
+    @FXML
+    public void onClickFitToContent(MouseEvent mouseEvent) {
+        getActiveTabController().onClickFitToContent();
+    }
+
+    @FXML
+    public void onClickResetZoom(MouseEvent mouseEvent) {
+        getActiveTabController().onClickResetZoom();
+    }
+
+    private DiagramController getActiveTabController() {
+        Tab tab = checkedOrSelected.getSelectionModel().getSelectedItem();
+        if ("Selected".equals(tab.getText())) {
+            return selectedDiagramController;
+        } else {
+            Tab tabInChecked = checkedTab.getSelectionModel().getSelectedItem();
+            return checkedDiagramControllers.get(tabInChecked);
+        }
+    }
+}
