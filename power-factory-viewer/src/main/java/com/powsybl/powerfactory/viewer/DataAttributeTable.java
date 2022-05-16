af80a3e317258a9adc0fde2df8d3ab83c11f37e0
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2022-05-16 19:00:25 +0200

    PowerFactory data viewer (#47)
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

---
+++ power-factory-viewer/src/main/java/com/powsybl/powerfactory/viewer/DataAttributeTable.java
@@ -0,0 +1,158 @@
+/**
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.powerfactory.viewer;
+
+import com.powsybl.powerfactory.model.DataAttribute;
+import com.powsybl.powerfactory.model.DataObject;
+import com.powsybl.powerfactory.model.DataObjectRef;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.event.ActionEvent;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.linear.RealMatrixFormat;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * @author Geoffroy Jamgotchian <geoffroy.jamgotchian at rte-france.com>
+ */
+public class DataAttributeTable extends BorderPane {
+
+    private static final RealMatrixFormat MATRIX_FORMAT = new RealMatrixFormat("{", "}", "{", "}", "," + System.lineSeparator(), ",");
+
+    private final TableView<DataAttribute> tableView;
+
+    private DataObject dataObject;
+
+    @SuppressWarnings("unchecked")
+    public DataAttributeTable(DataObjectTree dataObjectTree) {
+        Objects.requireNonNull(dataObjectTree);
+        tableView = new TableView<>();
+        tableView.setStyle("-fx-selection-bar: yellow; -fx-selection-bar-non-focused: lightyellow;");
+        setCenter(tableView);
+        TableColumn<DataAttribute, String> attrNameCol = new TableColumn<>("Name");
+        TableColumn<DataAttribute, String> attrTypeCol = new TableColumn<>("Type");
+        TableColumn<DataAttribute, Object> attrValueCol = new TableColumn<>("Value");
+        attrNameCol.setPrefWidth(200);
+        attrTypeCol.setPrefWidth(150);
+        attrValueCol.setPrefWidth(700);
+        attrNameCol.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getName()));
+        attrTypeCol.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getType().name()));
+        attrValueCol.setCellValueFactory(param -> {
+            Object value = getAttributeValue(param.getValue());
+            return value != null ? new SimpleObjectProperty<>(value) : null;
+        });
+        attrValueCol.setCellFactory(param -> new TableCell<>() {
+
+            private Node createHyperlink(DataObjectRef dataObjectRef) {
+                DataObject otherDataObject = dataObjectRef.resolve().orElse(null);
+                if (otherDataObject != null) {
+                    Hyperlink hyperlink = new Hyperlink(otherDataObject.toString());
+                    hyperlink.setTooltip(new Tooltip(otherDataObject.toString()));
+                    hyperlink.setOnAction((ActionEvent event) -> {
+                        dataObjectTree.search(otherDataObject);
+                        tableView.requestFocus();
+                    });
+                    return hyperlink;
+                } else {
+                    var label = new Label(Long.toString(dataObjectRef.getId()));
+                    label.setTextFill(Color.RED);
+                    return label;
+                }
+            }
+
+            private Node createHyperlinkList(List<DataObjectRef> dataObjectRefs) {
+                VBox vBox = new VBox();
+                Iterator<DataObjectRef> it = dataObjectRefs.iterator();
+                List<Node> nodes = new ArrayList<>();
+                nodes.add(new Text("["));
+                while (it.hasNext()) {
+                    DataObjectRef dataObjectRef = it.next();
+                    Node hyperlink = createHyperlink(dataObjectRef);
+                    nodes.add(hyperlink);
+                    if (it.hasNext()) {
+                        nodes.add(new Text(","));
+                    } else {
+                        nodes.add(new Text("]"));
+                    }
+                    HBox hBox = new HBox(nodes.toArray(new Node[0]));
+                    hBox.setAlignment(Pos.CENTER_LEFT);
+                    vBox.getChildren().add(hBox);
+                    nodes.clear();
+                }
+                return vBox;
+            }
+
+            @Override
+            public void updateItem(Object item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty) {
+                    setGraphic(null);
+                    setText(null);
+                } else {
+                    if (item != null) {
+                        if (item instanceof DataObjectRef) {
+                            Node hyperlink = createHyperlink((DataObjectRef) item);
+                            setGraphic(hyperlink);
+                            setText(null);
+                        } else if (item instanceof List && !((List<?>) item).isEmpty() && ((List<?>) item).get(0) instanceof DataObjectRef) {
+                            Node hyperlinks = createHyperlinkList((List<DataObjectRef>) item);
+                            setGraphic(hyperlinks);
+                            setText(null);
+                        } else if (item instanceof RealMatrix) {
+                            setGraphic(null);
+                            setText(MATRIX_FORMAT.format((RealMatrix) item));
+                        } else {
+                            String text = item.toString();
+                            // hack to display dates
+                            if (item instanceof Long
+                                    && ((Long) item) > 946684800L && ((Long) item) < 2208988800L) { // epoch between 2000 and 2040
+                                text += " (" + Instant.ofEpochSecond((Long) item) + ")";
+                            }
+                            setGraphic(null);
+                            setText(text);
+                        }
+                    } else {
+                        setGraphic(null);
+                        setText(null);
+                    }
+                }
+            }
+        });
+        tableView.getColumns().addAll(attrNameCol, attrTypeCol, attrValueCol);
+    }
+
+    private Object getAttributeValue(DataAttribute attribute) {
+        if (dataObject != null) {
+            return dataObject.findAttributeValue(attribute.getName()).orElse(null);
+        }
+        return null;
+    }
+
+    public void setDataObject(DataObject dataObject) {
+        this.dataObject = dataObject;
+        if (dataObject != null) {
+            tableView.getItems().setAll(dataObject.getDataClass().getAttributes());
+            tableView.scrollTo(0);
+            tableView.getSelectionModel().select(0);
+        } else {
+            tableView.getItems().clear();
+        }
+    }
+}
---
+++ power-factory-viewer/src/main/java/com/powsybl/powerfactory/viewer/PowerFactoryViewer.java
@@ -0,0 +1,168 @@
+/**
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.powerfactory.viewer;
+
+import com.powsybl.powerfactory.model.PowerFactoryDataLoader;
+import com.powsybl.powerfactory.model.PowerFactoryException;
+import com.powsybl.powerfactory.model.Project;
+import javafx.application.Application;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.control.SplitPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
+import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.prefs.Preferences;
+import java.util.stream.Collectors;
+
+/**
+ * @author Geoffroy Jamgotchian <geoffroy.jamgotchian at rte-france.com>
+ */
+public class PowerFactoryViewer extends Application {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PowerFactoryViewer.class);
+
+    private static final String FILE_PATH_PROPERTY = "filePath";
+
+    private final TextField filePathTextField = new TextField();
+
+    private final FileChooser fileChooser = new FileChooser();
+
+    private final DataObjectTree dataObjectTree = new DataObjectTree();
+
+    private final DataAttributeTable dataAttributeTable = new DataAttributeTable(dataObjectTree);
+
+    private final Preferences preferences = Preferences.userNodeForPackage(DataObjectTree.class);
+
+    private final BorderPane mainPane = new BorderPane();
+
+    private final ProgressIndicator progressIndicator = new ProgressIndicator();
+
+    private final Group loader = new Group(progressIndicator);
+
+    private Service<Project> service;
+
+    private void loadFileFromPreferences() {
+        String pathPropertyValue = preferences.get(FILE_PATH_PROPERTY, null);
+        if (pathPropertyValue != null) {
+            loadFile(Paths.get(pathPropertyValue));
+        }
+    }
+
+    private void showLoader() {
+        loader.setVisible(true);
+        mainPane.setDisable(true);
+    }
+
+    private void hideLoader() {
+        loader.setVisible(false);
+        mainPane.setDisable(false);
+    }
+
+    private void loadFile(Path file) {
+        service = new Service<>() {
+            @Override
+            protected Task<Project> createTask() {
+                return new Task<>() {
+                    @Override
+                    protected Project call() {
+                        return PowerFactoryDataLoader.load(file, Project.class)
+                                .orElseThrow(() -> new PowerFactoryException("Cannot load file '" + file + "'"));
+                    }
+                };
+            }
+        };
+
+        service.setOnScheduled(event -> {
+            dataObjectTree.setProject(null);
+            showLoader();
+        });
+
+        service.setOnSucceeded(event -> {
+            Project project = (Project) event.getSource().getValue();
+            dataObjectTree.setProject(project);
+            hideLoader();
+        });
+
+        service.setOnFailed(event -> {
+            LOGGER.error(event.getSource().getException().toString(), event.getSource().getException());
+            dataObjectTree.setProject(null);
+            hideLoader();
+        });
+
+        service.start();
+
+        filePathTextField.setText(file.toAbsolutePath().toString());
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        dataObjectTree.selectedDataObjectProperty().addListener((observable, oldValue, newValue) -> dataAttributeTable.setDataObject(newValue));
+
+        fileChooser.setTitle("Open PowerFactory file");
+        List<String> extensions = PowerFactoryDataLoader.find(Project.class).stream()
+                .map(l -> "*." + l.getExtension())
+                .collect(Collectors.toList());
+        String filterDescr = "PowerFactory files (" + String.join(", ", extensions) + ")";
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter(filterDescr, extensions));
+        filePathTextField.setEditable(false);
+        HBox.setHgrow(filePathTextField, Priority.ALWAYS);
+
+        Button filePathButton = new Button("...");
+        filePathButton.setOnAction(event -> {
+            String pathPropertyValue = preferences.get(FILE_PATH_PROPERTY, null);
+            if (pathPropertyValue != null) {
+                fileChooser.setInitialDirectory(Paths.get(pathPropertyValue).getParent().toFile());
+            }
+
+            File file = fileChooser.showOpenDialog(primaryStage);
+            if (file != null) {
+                loadFile(file.toPath());
+                preferences.put(FILE_PATH_PROPERTY, file.getAbsolutePath());
+            }
+        });
+        HBox filePathPane = new HBox(3, filePathTextField, filePathButton);
+        Text dataObjPane = new Text();
+        dataObjectTree.selectedDataObjectProperty().addListener((observable, oldValue, newValue) -> dataObjPane.setText(newValue != null ? newValue.toString() : ""));
+        VBox topPane = new VBox(filePathPane, dataObjPane);
+        BorderPane.setMargin(topPane, new Insets(3, 3, 3, 3));
+
+        SplitPane dataPane = new SplitPane(dataObjectTree, dataAttributeTable);
+        dataPane.setDividerPositions(0.3);
+        mainPane.setCenter(dataPane);
+        mainPane.setTop(topPane);
+
+        hideLoader();
+        StackPane stackPane = new StackPane(mainPane, loader);
+
+        Scene scene = new Scene(stackPane, 1000, 800);
+        primaryStage.setTitle("PowerFactory Viewer");
+        primaryStage.setScene(scene);
+
+        loadFileFromPreferences();
+
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
