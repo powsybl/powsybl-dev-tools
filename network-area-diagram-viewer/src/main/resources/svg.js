886ee07e4a26315aff01cf7f44ab638fa504e3f0
Florian Dupuy <florian.dupuy@rte-france.com>
2023-01-03 16:45:54 +0100

    Add license header
    Signed-off-by: Florian Dupuy <florian.dupuy@rte-france.com>

--- network-area-diagram-viewer/src/main/resources/svg.js
+++ network-area-diagram-viewer/src/main/resources/svg.js
@@ -1,4 +1,10 @@
-
+/**
+ * Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * SPDX-License-Identifier: MPL-2.0
+ */
 // TODO(Luma) change text orientation or hide node infos if rotation makes them appear inverted
 // TODO(Luma) how to handle multi-lines without having to split stretchable and glued to end? (avoid gaps and/or overlaps)
 // TODO(Luma) redraw the annuli
