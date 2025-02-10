9d9db6b1bbadf195741119e8978ff984bcb1f49a
Naledi EL CHEIKH <naledi.elcheikh@rte-france.com>
2025-02-10 14:29:54 +0100

    add : get messages templates, sorted and unique in text file
    Signed-off-by: Naledi EL CHEIKH <naledi.elcheikh@rte-france.com>

--- scripts/get_message_templates.sh
+++ scripts/get_message_templates.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+path="$1"
 function getMessageTemplates() {
   echo
   echo
-  grep --color='auto' -R "newReportNode().withMessageTemplate" --exclude="*Test*" --exclude="*Reports.java" /.
+  grep --color='auto' -R "newReportNode().withMessageTemplate" "$path" | awk -F '"' '{print $2, $3, $4}' | sort -g | uniq | sed "s/,/=/g" > message_templates.txt
   echo
 }
 
 function main() {
   echo
+  getMessageTemplates
+  echo "INFO : message_templates.txt generated successfully"
 }
 
 main
