c18a05139fef8ffd1ba919e99d5af0fb0f9414f1
Naledi EL CHEIKH <naledi.elcheikh@rte-france.com>
2025-02-13 10:49:11 +0100

    update : duplicated keys display, and fixed properties file syntax
    Signed-off-by: Naledi EL CHEIKH <naledi.elcheikh@rte-france.com>

--- scripts/get_message_templates.sh
+++ scripts/get_message_templates.sh
@@ -2,10 +2,11 @@
 
 path="$1"
 function getMessageTemplates() {
-  grep -R "\\.withMessageTemplate" --exclude="*Test*" --exclude="*.class" "$path" | awk -F'"' '{sub(/[ \t]+$/, "", $2); sub(/[ \t]+$/, "", $3); print $2, $3, $4}' | sort -g | uniq -u | sed "s/,/=/g" > dictionary.properties
-  echo "==== Duplicated lines ===="
-  grep -R "\\.withMessageTemplate" --exclude="*Test*" --exclude="*.class" "$path" | awk -F'"' '{sub(/[ \t]+$/, "", $2); sub(/[ \t]+$/, "", $3); print $2, $3, $4}' | sort -g | uniq -d -c | sed "s/,/=/g"
+  grep -R "\\.withMessageTemplate" --exclude="*Test*" --include="*.java" "$path" | awk -F'"' '{sub(/[ \t]+$/, "", $2); sub(/[ \t]+$/, "", $3); print $2, $3, $4}' | sort -g | sed 's/,/=/' > temp.properties
+  echo "==== Duplicated keys ===="
+  awk 'NF {print $1}' temp.properties | sort -g | uniq -d -c
   echo "==== End ===="
+  sort -u -t= -k1,1 temp.properties > dictionary.properties ; rm temp.properties
   chmod u+x dictionary.properties
 }
 
