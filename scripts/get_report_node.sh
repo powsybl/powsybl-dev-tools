03432aba3a8b793787afa85148993f6cd5aa78da
Naledi <151443525+nao1345678@users.noreply.github.com>
2025-02-24 15:25:50 +0100

    Scripts for ReportNodes internationalization (#121)
    * add : shell file for getting report node calls out of reports.java files
    * update : added file parameter in getReportNode
    * add : get messages templates, sorted and unique in text file
    * update : count amount of duplication
    * fix : output of the script
    * update : duplicated keys display, and fixed properties file syntax
    * update : script modification to match source code
    * update : added potential errors handling by user & u+x permissions
    
    Signed-off-by: Naledi EL CHEIKH <naledi.elcheikh@rte-france.com>

---
+++ scripts/get_report_node.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+path="$1"
+
+function getReportNode() {
+  grep --color='auto' -R "\\.withMessageTemplate" --include="*.java" --exclude="*Test*" --exclude="*Reports.java" "$path"
+}
+
+function main() {
+  echo -e "\033[35;1;4m=== ReportNode calls out of ...Reports.java files ===\033[0m"
+  getReportNode
+  echo "==== End ===="
+
+}
+
+main
