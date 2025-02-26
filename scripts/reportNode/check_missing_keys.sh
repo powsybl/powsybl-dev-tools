8f14fc8eaa793f00dc2fa157160e92ddb59cf212
Olivier Perrin <olivier.perrin@rte-france.com>
2025-02-26 18:13:03 +0100

    Add check_missing_keys.sh
    Signed-off-by: Olivier Perrin <olivier.perrin@rte-france.com>

---
+++ scripts/reportNode/check_missing_keys.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <directory> <dictionary.properties>"
+    exit -1
+fi
+
+path="$1"
+dictionary="$2"
+
+function getReportNode() {
+  grep -h -A 1 -R "\\.withLocaleMessageTemplate(" --include="*.java" --exclude="*Test*" "$path" | \
+     pcregrep -M "\.withLocaleMessageTemplate\((.|\n)*?\)" | \
+     awk '{ if (prev) $0 = prev " " $0; if ($0 ~ /[,+]$/) { prev = $0; next } prev = ""; print $0 } END { if (prev) print prev }' | \
+     awk -F'"' '{sub(/[ \t]+$/, "", $2); print $2}' | \
+     sort -g > /tmp/found_keys
+
+     diff "$dictionary" /tmp/found_keys | grep -E "^>" | cut -c 3-
+
+}
+
+function main() {
+  echo -e "\033[35;1;4m=== Missing keys ===\033[0m"
+  getReportNode
+  echo "==== End ===="
+  rm /tmp/found_keys
+}
+
+main
