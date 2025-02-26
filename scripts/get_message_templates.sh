b44b694053dab2956e5c506edc07933a27b2d3e4
Olivier Perrin <olivier.perrin@rte-france.com>
2025-02-26 09:10:06 +0100

    Quality
    Signed-off-by: Olivier Perrin <olivier.perrin@rte-france.com>

--- scripts/get_message_templates.sh
+++ scripts/get_message_templates.sh
@@ -9,16 +9,17 @@ path="$1"
 
 function getMessageTemplates() {
   grep -h -A 1 -R "\\.withMessageTemplate" --include="*.java" --exclude="*Test*" "$path" | \
-  pcregrep -M "\.withMessageTemplate\((.|\n)*?\)" | \
-  awk '{ if (prev) $0 = prev " " $0; if ($0 ~ /[,+]$/) { prev = $0; next } prev = ""; print $0 } END { if (prev) print prev }' | \
-  awk -F'"' '{sub(/[ \t]+$/, "", $2); sub(/[ \t]+$/, "", $3); sub(/^[ \t\r\n]+/, "", $4); sub(/^[ \t\r\n]+/, "", $6); print $2, $3, $4, $6}' | \
-  sed 's/,/=/' > temp.properties
+     pcregrep -M "\.withMessageTemplate\((.|\n)*?\)" | \
+     awk '{ if (prev) $0 = prev " " $0; if ($0 ~ /[,+]$/) { prev = $0; next } prev = ""; print $0 } END { if (prev) print prev }' | \
+     awk -F'"' '{sub(/[ \t]+$/, "", $2); sub(/[ \t]+$/, "", $3); sub(/^[ \t\r\n]+/, "", $4); sub(/^[ \t\r\n]+/, "", $6); print $2, $3, $4, $6}' | \
+     sed 's/,/=/' | \
+     awk '{$1=$1};1' > temp.properties
   echo "==== Duplicated keys ===="
   awk 'NF {print $1}' temp.properties | sort -g | uniq -d -c
   echo
   echo "==== Potential errors - Please check those lines ===="
   grep "\\\\" temp.properties
-  grep -v "=" temp.properties | grep -vE "^   $"
+  grep -v "=" temp.properties | grep -vE "^( )*$"
   echo
   echo "==== End ===="
   sort -u -t= -k1,1 temp.properties > dictionary.properties ; rm temp.properties
